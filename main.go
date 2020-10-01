1f547e4e16d476fbd8d5ed20bd7069263c82b5ab
Juno Yu <jxy35@hitit.xyz>
2020-10-01 15:22:04 +0900

    Dictionary search, add method implementation

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 
-	"github.com/mannerism/learngo/accounts"
+	"github.com/mannerism/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("juno")
-	account.Deposit(10)
-	fmt.Println(account)
+	// Account
+	// account := accounts.NewAccount("juno")
+	// account.Deposit(10)
+	// fmt.Println(account)
+
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	hello, err := dictionary.Search(word)
+	fmt.Println(hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
